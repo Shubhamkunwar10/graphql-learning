e277331952a75c820cba1818a550d70f5f11d2a9
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-06-30 12:47:30 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { PostCard } from '../components';
+import { getPosts } from '../services';
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+
